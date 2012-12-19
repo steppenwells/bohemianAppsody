9c6b2e831471030e8ca1447e1cdb0a90887149a5
steppenwells <steppenwells@gmail.com>
2012-12-19 09:04:52 +0000

    adds more job manager stuff

--- src/main/scala/com/swells/ba/service/JobManager.scala
+++ src/main/scala/com/swells/ba/service/JobManager.scala
@@ -6,11 +6,33 @@ object JobManager {
   var jobQueue: List[Job] = Nil
   var runningJobs: Map[String, Job] = Map()
 
+  def enqueue(job: Job) {
+    enqueue(List(job))
+  }
+
+  def enqueue(jobs: List[Job]) {
+    synchronized {
+      jobQueue = jobQueue ::: jobs
+    }
+  }
+
+  def getJob = synchronized {
+    jobQueue match {
+      case j :: js => {
+        jobQueue = js
+        runningJobs = runningJobs + (j.id, j)
+        Some(j)
+      }
+      case Nil => None
+    }
+  }
+
   def finished(id: String) {
     synchronized{
       runningJobs = runningJobs - id
     }
   }
+
   def errored(id: String) {
     synchronized {
       val failedJob = runningJobs(id)
